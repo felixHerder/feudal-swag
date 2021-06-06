91fe613ee5b4e5f04e9841a3bcb03f91330adedf
felixHerder <felixbaciu@gmail.com>
2021-06-06 13:54:19 +0300

    add cart selectors for redux state changes

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,29 @@
-import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
 
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/car-item.component';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
 
 
-const CartDropDown = () =>(
+const CartDropDown = ({cartItems}) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {
+        cartItems.map(cartItem=>(
+          <CartItem key={cartItem.id} item={cartItem}/>
+        ))
+      }
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
 
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
+});
 
-export default CartDropDown; 
+export default connect(mapStateToProps)(CartDropDown);
